efe8956b7034b1148210909d9d7bcb1a77eaaad7
Danny López Cárdenas <danny.lopezc00@gmail.com>
2023-05-06 08:29:16 -0500

    Drawing Rects

--- Assets/Scripts/GeneralizedAnimation.cs
+++ Assets/Scripts/GeneralizedAnimation.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Doozy.Runtime.Reactor.Animators;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,22 +7,49 @@ public class GeneralizedAnimation : EditorWindow
 {
     public float animationTime;
 
-    public List<GameObject> animations;
+    public List<UIAnimator> animations;
+    public List<Rect> rects;
     private Vector2 scrollPosition = Vector2.zero;
-    private GUIStyle scrollViewStyle;
-
 
     // variables para el foldout
-    private bool isFoldoutOpen;
+    private bool isListFoldoutOpen;
+    private bool isRectsFoldoutOpen;
 
     private void OnGUI()
     {
         animationTime = EditorGUILayout.FloatField("Animation Time", animationTime);
 
-        CreateAnimationsList();
+        Vector2 lastItemPosition;
+        CreateAnimationsList(out lastItemPosition);
 
         DragAction();
+
+        DrawRects(lastItemPosition);
     }
+
+    private void DrawRects(Vector2 lastItemPosition)
+    {
+        rects.Clear();
+        const float height = 30;
+        float width = 100;
+        const float xPos = 10;
+
+        const float offSetBetween = (height / 2) + 10;
+
+        isRectsFoldoutOpen = EditorGUILayout.Foldout(isRectsFoldoutOpen, "Timeline");
+
+        if (isRectsFoldoutOpen)
+        {
+            for (int i = 0; i < animations.Count; i++)
+            {
+                Rect rect = new Rect(xPos, lastItemPosition.y + offSetBetween * i + (height / 2) * i, width, height);
+                GUI.Box(rect, "hello");
+
+                rects.Add(rect); // Agrega el rect generado a la lista
+            }
+        }
+    }
+
 
     private void DragAction()
     {
@@ -37,9 +65,11 @@ public class GeneralizedAnimation : EditorWindow
             DragAndDrop.AcceptDrag();
             foreach (Object draggedObject in DragAndDrop.objectReferences)
             {
-                if (draggedObject is GameObject)
+                if (draggedObject is not GameObject) continue;
+                GameObject go = draggedObject as GameObject;
+                if (go.TryGetComponent(out UIAnimator animator))
                 {
-                    animations.Add(draggedObject as GameObject);
+                    animations.Add(animator);
                 }
             }
 
@@ -47,16 +77,18 @@ public class GeneralizedAnimation : EditorWindow
         }
     }
 
-    private void CreateAnimationsList()
+    private float CreateAnimationsList(out Vector2 lastItemPosition)
     {
         // crear el foldout
-        isFoldoutOpen = EditorGUILayout.Foldout(isFoldoutOpen, "Animations List");
+        isListFoldoutOpen = EditorGUILayout.Foldout(isListFoldoutOpen, "Animations List");
+        float maxScrollHeight = 100f; // valor máximo de altura del scroll
+        float totalHeight = 0f; // altura total de los objetos
+        float scrollHeight = 0f;
 
-        if (isFoldoutOpen)
+
+        if (isListFoldoutOpen)
         {
-            float maxScrollHeight = 100f; // valor máximo de altura del scroll
-            float totalHeight = 0f; // altura total de los objetos
-            foreach (GameObject obj in animations)
+            foreach (UIAnimator obj in animations)
             {
                 totalHeight += EditorGUIUtility.singleLineHeight + 7f;
             }
@@ -66,8 +98,9 @@ public class GeneralizedAnimation : EditorWindow
                 totalHeight = EditorGUIUtility.singleLineHeight;
             }
 
-            float scrollHeight = Mathf.Min(totalHeight, maxScrollHeight);
-            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUI.skin.box, GUILayout.Height(scrollHeight));
+            scrollHeight = Mathf.Min(totalHeight, maxScrollHeight);
+            scrollPosition =
+                EditorGUILayout.BeginScrollView(scrollPosition, GUI.skin.box, GUILayout.Height(scrollHeight));
 
             // Add space to the left
             GUILayout.BeginHorizontal();
@@ -78,8 +111,9 @@ public class GeneralizedAnimation : EditorWindow
             // Draw list items
             for (int i = 0; i < animations.Count; i++)
             {
-                animations[i] = (GameObject)EditorGUILayout.ObjectField("Animation " + (i + 1).ToString(), animations[i],
-                    typeof(GameObject), true);
+                animations[i] = (UIAnimator)EditorGUILayout.ObjectField("Animation " + (i + 1).ToString(),
+                    animations[i],
+                    typeof(UIAnimator), true);
             }
 
             // End layout group
@@ -109,6 +143,9 @@ public class GeneralizedAnimation : EditorWindow
 
             GUILayout.EndHorizontal();
         }
+
+        lastItemPosition = new Vector2(20, 90 + scrollHeight); // posición del último elemento
+        return scrollHeight;
     }
 
     [MenuItem("Window/Generalized Animation")]
